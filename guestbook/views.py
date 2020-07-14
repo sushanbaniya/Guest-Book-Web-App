19440c6ed7383a95ea2e8039eb45ec5d7e87cd48
sushanbaniya <sushanbaniya@gmail.com>
2020-07-14 13:42:35 +0545

    Initial Commit

---
+++ guestbook/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from .models import Comment
+from .forms import CommentForm
+
+
+# Create your views here.
+
+def index(request):
+    comment = Comment.objects.order_by('-date_added')
+
+    context = {'comment': comment}
+
+    return render(request, 'guestbook/index.html', context)
+
+
+def sign(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = Comment(name=request.POST['name'], comment=request.POST['comment'])
+            new_comment.save()
+            return redirect('index')
+
+    else:
+        form = CommentForm()
+
+    context = {'form': form}
+
+    return render(request, 'guestbook/sign.html', context)
